19bab6655c7922b35e857cce4625ff38c3e36cbd
Mikhail Drobynin <mdrobynin@griddynamics.net>
2019-09-17 18:57:35 +0400

    added decorator

--- src/structural/Decorator.js
+++ src/structural/Decorator.js
@@ -1,3 +1,34 @@
 export function Decorator(logger) {
+    const item = new Item('some value', logger);
+    const decoratedItem = new DecoratedItem(item, 'some additional value', logger);
 
+    item.log();
+    decoratedItem.log();
+}
+
+class Item {
+    constructor(value, logger) {
+        this.value = value;
+        this.logger = logger;
+
+        this.logger.add('Item: Created instance of Item with value: ' + value);
+    }
+
+    log() {
+        this.logger.add('Item: Called method of Item with value: ' + this.value);
+    }
+}
+
+class DecoratedItem {
+    constructor(item, someAdditionalValue, logger) {
+        this.item = item;
+        this.someAdditionalValue = someAdditionalValue;
+        this.logger = logger;
+
+        this.logger.add('DecoratedItem: Created instance of DecoratedItem with Item value: ' + item.value + ' and additional value ' + this.someAdditionalValue);
+    }
+
+    log() {
+        this.logger.add('DecoratedItem: Called method of DecoratedItem with value: ' + this.item.value+ ' and additional value ' + this.someAdditionalValue);
+    }
 }
