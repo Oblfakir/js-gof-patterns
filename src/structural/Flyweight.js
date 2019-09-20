97202670a19d8e61cddb0cde1577274d7898c914
Mikhail Drobynin <mdrobynin@griddynamics.net>
2019-09-20 10:42:25 +0400

    added flyweight and proxy

--- src/structural/Flyweight.js
+++ src/structural/Flyweight.js
@@ -1,14 +1,111 @@
 export function Flyweight(logger) {
     showDescription();
+
+    const items = new ItemCollection(logger);
+
+    items.add('Source 1', 'value 1', 'some property', 'hsf834fdn3');
+    items.add('Source 1', 'value 1', 'some property', 'b655895323');
+    items.add('Source 1', 'value 1', 'some property', 'hjcfewj234');
+    items.add('Source 1', 'value 1', 'some property', 'nx23405vj8');
+    items.add('Source 2', 'value 1', 'some property', 'fmrc5jvg40');
+    items.add('Source 2', 'value 1', 'some property', 'xm239mc494');
+    items.add('Source 2', 'value 2', 'some property', 'qcn34gf5sx');
+    items.add('Source 2', 'value 2', 'some property', 'fmrc5jvg40');
+    items.add('Source 2', 'value 2', 'some property', 'xm239mc494');
+    items.add('Source 2', 'value 2', 'some property', 'qcn34gf5sx');
+
+    logger.add(`Item Collection length is: ${ items.getCount()}`);
+    logger.add(`Flyweights count is: ${ ItemFlyweightFactory.getCount(logger)}`);
 }
+
+class ItemFlyweight {
+    constructor(source, value, property, logger) {
+        this.source = source;
+        this.value = value;
+        this.property = property;
+
+        logger.add(`Item: Created instance of ItemFlyweight with args: ${source}, ${value}, ${property}`);
+    }
+}
+
+class Item {
+    constructor(source, value, property, id, logger) {
+        this.flyweight = ItemFlyweightFactory.get(source, value, property, logger);
+        this.id = id;
+        this.logger = logger;
+
+        logger.add(`Item: Created instance of Item with args: ${source}, ${value}, ${property}, ${id}`);
+    }
+
+    getProperty() {
+        logger.add('Item: Called getProperty method');
+        return this.flyweight.property;
+    }
+}
+
+class ItemCollection {
+    constructor(logger) {
+        this.logger = logger;
+        this.items = {};
+        this.count = 0;
+
+        logger.add('ItemCollection: Created instance of ItemCollection');
+    }
+
+    add(source, value, property, id) {
+        this.logger.add('ItemCollection: Called add method');
+        this.items[id] = new Item(source, value, property, id, this.logger);
+        this.count ++;
+    }
+    
+    get(id) {
+        this.logger.add('ItemCollection: Called get method');
+        return this.items[id];
+    }
+
+    getCount() {
+        this.logger.add('ItemCollection: Called getCount method');
+        return this.count;
+    }
+}
+
+const ItemFlyweightFactory = (() => {
+    const flyweights = {};
+
+    return {
+        get(source, value, property, logger) {
+            logger.add(`ItemFlyweightFactory: Called get method with source ${source} and value ${value}`);
+
+            if (!flyweights[source + value]) {
+                logger.add(`ItemFlyweightFactory: No flyweight found with source ${source} and value ${value}, creating one`);
+                flyweights[source + value] = new ItemFlyweight(source, value, property, logger);
+            }
+
+            return flyweights[source + value];
+        },
+        getCount(logger) {
+            logger.add(`ItemFlyweightFactory: Called getCount method`);
+            return Object.keys(flyweights).length;
+        }
+    };
+})();
 
 function showDescription() {
     document.getElementById('description').textContent = '';
 
-    [``
-    ,``
-    ,``
-    ,``]
+    [`The Flyweight pattern conserves memory by sharing large numbers of fine-grained objects 
+    efficiently. Shared flyweight objects are immutable, that is, they cannot be changed 
+    as they represent the characteristics that are shared with other objects.`
+    ,`Essentially Flyweight is an 'object normalization technique' in which common 
+    properties are factored out into shared flyweight objects. (Note: the idea is similar
+         to data model normalization, a process in which the modeler attempts to minimize
+          redundancy).`
+    ,`An example of the Flyweight Pattern is within the JavaScript engine itself which 
+    maintains a list of immutable strings that are shared across the application.`
+    ,`Other examples include characters and line-styles in a word processor, or 'digit 
+    receivers' in a public switched telephone network application. You will find flyweights
+     mostly in utility type applications such as word processors, graphics programs, 
+     and network apps; they are less often used in data-driven business type applications.`]
         .map(x => {
             const p = document.createElement('p');
             p.textContent = x;
--- src/structural/Proxy.js
+++ src/structural/Proxy.js
@@ -1,14 +1,96 @@
 export function Proxy(logger) {
     showDescription();
+
+    const itemProxy = new ItemProxy(logger);
+
+    itemProxy.getItem(1);
+    itemProxy.getItem(2);
+    itemProxy.getItem(3);
+    itemProxy.getItem(4);
+    itemProxy.getItem(5);
+    itemProxy.getItem(2);
+    itemProxy.getItem(3);
+    itemProxy.getItem(2);
+    itemProxy.getItem(4);
+    itemProxy.getItem(4);
+    itemProxy.getItem(4);
+    itemProxy.getItem(4);
+
+    logger.add('Proxy cache size: ' + itemProxy.getCount());
+}
+
+class ItemProxy {
+    constructor(logger) {
+        this.logger = logger;
+        this.itemProvider = new ItemProvider(logger);
+        this.cache = {};
+
+        this.logger.add('ItemProxy: Created instance of ItemProxy');
+    }
+
+    getItem(id) {
+        if (!this.cache[id]) {
+            this.cache[id] = this.itemProvider.getItem(id);
+        }
+
+        this.logger.add('ItemProxy: Requested item with id: ' + id);
+
+        return this.cache[id];
+    }
+
+    getCount() {
+        this.logger.add('ItemProxy: Requested item count');
+
+        return Object.keys(this.cache).length;
+    }
+}
+
+class ItemProvider {
+    constructor(logger) {
+        this.logger = logger;
+        this.items = [];
+
+        this.logger.add('ItemProvider: Created instance of ItemProvider');
+    }
+
+    getItem(id) {
+        this.logger.add('ItemProvider: Requested item with id: ' + id);
+        const item = this.items.find(x => x.id === id);
+
+        if (item) return item;
+
+        const newItem = new Item('value ' + id, id, this.logger)
+        this.items.push(newItem);
+
+        return newItem;
+    }
+}
+
+class Item {
+    constructor(value, id, logger) {
+        this.value = value;
+        this.id = id;
+        this.logger = logger;
+
+        this.logger.add('Item: Created instance of Item');
+    }
 }
 
 function showDescription() {
     document.getElementById('description').textContent = '';
 
-    [``
-    ,``
-    ,``
-    ,``]
+    [`The Proxy pattern provides a surrogate or placeholder object for another 
+    object and controls access to this other object.`
+    ,`In object-oriented programming, objects do the work they advertise through 
+    their interface (properties and methods). Clients of these objects expect this 
+    work to be done quickly and efficiently. However, there are situations where 
+    an object is severely constrained and cannot live up to its responsibility. 
+    Typically this occurs when there is a dependency on a remote resource (resulting 
+        in network latency) or when an object takes a long time to load.`
+    ,`In situations like these you apply the Proxy pattern and create a proxy object 
+    that ‘stands in’ for the original object. The Proxy forwards the request to a 
+    target object. The interface of the Proxy object is the same as the original object 
+    and clients may not even be aware they are dealing with a proxy rather than the real object.`]
         .map(x => {
             const p = document.createElement('p');
             p.textContent = x;
